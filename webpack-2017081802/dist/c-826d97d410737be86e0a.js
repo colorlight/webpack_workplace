725f621b14e584858d112d4e46c112e5cfca972d
colorlight <colorlightleecaa@gmail.com>
2017-08-21 19:03:28 +0800

    multi htmls

--- webpack-2017081802/dist/c-826d97d410737be86e0a.js
+++ webpack-2017081802/dist/c-826d97d410737be86e0a.js
@@ -60,16 +60,16 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 8);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 7:
+/***/ 8:
 /***/ (function(module, exports) {
 
-function a(){
-  
+function sdfsf() {
+
 }
 
 
--- webpack-2017081802/webpack.config.js
+++ webpack-2017081802/webpack.config.js
@@ -5,21 +5,40 @@ module.exports =  {
   // context: __dirname,
   entry: {
     hello: './src/script/hello.js',
-    a: './src/script/a.js'
+    a: './src/script/a.js',
+    b: './src/script/buffer.js',
+    c: './src/script/c.js'
   },
 
   output: {
     path: path.resolve(__dirname,'./dist/'),
-    filename: '[chunkhash].js'
+    filename: '[name]-[chunkhash].js'
   },
 
   plugins: [
     new htmlWebpackPlugin({
-      filename: 'html/index-[hash].html',
+      filename: 'html/a.html',
       template: 'index.html',
-      inject: false,
+      inject: 'head',
       title: '分仓库有',
-      date: new Date()
+      date: new Date(),
+      chunks:['a', 'hello']
+    }),
+    new htmlWebpackPlugin({
+      filename: 'html/b.html',
+      template: 'index.html',
+      inject: 'head',
+      title: '分仓库有',
+      date: new Date(),
+      chunks:['b', 'hello']
+    }),
+    new htmlWebpackPlugin({
+      filename: 'html/c.html',
+      template: 'index.html',
+      inject: 'head',
+      title: '分仓库有',
+      date: new Date(),
+      chunks:['c']
     })
   ]
 }
